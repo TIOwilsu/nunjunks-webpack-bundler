6df68a3bee6676d3479e3cf38357e6595668e5d6
Wilson Queiroz Rocha <wilson.queiroz@outlook.com>
2024-07-29 08:36:33 -0300

    chore: :wrench: config indent files

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,24 @@
-const globals = require('globals',);
-const path = require('node:path',);
-const js = require('@eslint/js',);
-const { FlatCompat, } = require('@eslint/eslintrc',);
+const globals = require('globals');
+const html = require('@html-eslint/eslint-plugin');
+const js = require('@eslint/js');
+const scss = require('stylelint-scss');
+const standardScss = require('stylelint-config-standard-scss');
+const prettier = require('eslint-plugin-prettier');
+const jsonc = require('eslint-plugin-jsonc');
+const prettierConfig = require('eslint-config-prettier');
+const { FlatCompat } = require('@eslint/eslintrc');
 
-const dirname = path.dirname(__dirname,);
-const compat = new FlatCompat({
-  baseDirectory: dirname,
+const { DIR } = require('./webpack.helpers');
+
+const COMPAT_JS = new FlatCompat({
+  baseDirectory: DIR,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
-},);
+});
 
 module.exports = [
+  ...COMPAT_JS.extends('eslint:recommended'),
   {
-    ...compat.extends('eslint:recommended',),
     languageOptions: {
       globals: {
         ...globals.node,
@@ -28,16 +34,29 @@ module.exports = [
     },
   },
   {
+    files: ['**/*.{scss,json}'],
+    plugins: {
+      prettier: prettier,
+    },
     rules: {
-      'array-bracket-spacing': ['error', 'never',],
-      'arrow-parens': ['error', 'always',],
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ...prettierConfig,
+  },
+  {
+    files: ['*.js'],
+    rules: {
+      'array-bracket-spacing': ['error', 'never'],
+      'arrow-parens': ['error', 'always'],
       'no-undef': 0,
       'no-console': 0,
       eqeqeq: 0,
       strict: 0,
-      semi: ['error', 'always',],
-      'eol-last': ['error', 'always',],
-      'comma-dangle': ['error', 'always',],
+      semi: ['error', 'always'],
+      'eol-last': ['error', 'always'],
+      'comma-dangle': 'off',
       curly: 'error',
       'no-else-return': 'error',
       'dot-notation': 'error',
@@ -56,7 +75,7 @@ module.exports = [
         },
       ],
 
-      indent: ['error', 2,],
+      indent: ['error', 2],
       'no-var': 'error',
 
       'no-unused-vars': [
@@ -70,8 +89,35 @@ module.exports = [
       'no-extra-boolean-cast': 'error',
       'no-irregular-whitespace': 'error',
       'valid-typeof': 'error',
-      quotes: ['error', 'single',],
-      'space-before-function-paren': ['error', 'always',],
+      quotes: ['error', 'single'],
+      'space-before-function-paren': ['error', 'always'],
+    },
+  },
+  {
+    ...html.configs['flat/recommended'],
+    files: ['**/*.html'],
+    rules: {
+      ...html.configs['flat/recommended'].rules,
+      '@html-eslint/indent': ['error', 2],
+      '@html-eslint/require-closing-tags': 'off',
+    },
+  },
+  {
+    files: ['**/*.scss'],
+    extends: [standardScss],
+    plugins: [scss],
+  },
+  {
+    files: ['**/*.css'],
+    extends: ['stylelint-config-standard'],
+  },
+  {
+    files: ['**/*.json', '**/*.jsonc'],
+    languageOptions: {
+      parser: jsonc.parsers.jsonc,
+    },
+    plugins: {
+      jsonc,
     },
   },
 ];
